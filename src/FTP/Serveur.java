22eb27ceae331bffd05552874177cea2f0a7476a
Bounehar Lynda <lynda.bounehar.etu@b12p14.fil.univ-lille.fr>
2024-01-11 17:17:06 +0100

    commit

--- src/FTP/Serveur.java
+++ src/FTP/Serveur.java
@@ -1,4 +1,4 @@
-package FTP ;
+package FTP;
 import java.io.*;
 import java.net.*;
 import java.util.Scanner;
@@ -15,55 +15,32 @@ public class Serveur {
 
             System.out.println("Serveur FTP démarré sur le port " + port + "...");
 
-            while (true) {
-                Socket clientSocket = serverSocket.accept();
-                System.out.println("Nouvelle connexion FTP reçue");
+            
 
-                // Créez un nouveau thread pour gérer la connexion
-                Thread clientThread = new Thread(() -> handleClient(clientSocket));
-                clientThread.start();
+                while (true) { 
+                    Socket clientSocket = serverSocket.accept();
+                    OutputStream out =clientSocket.getOutputStream();
+                    String str="200 Service ready\r\n";
+                    out.write(str.getBytes());
+                  
+                    InputStream in = clientSocket.getInputStream();
+                    
+                    Scanner scanner = new Scanner(in);
+
+
+                    String userName= scanner.nextLine();
+                    System.out.println(userName);
+                    out.write("331 User name ok \r\n".getBytes());
+                 
+
+                    String pwd= scanner.nextLine();
+                    out.write("230 User logged in \r\n".getBytes());
+                    System.out.println(pwd);
+
+                   scanner.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
     }
-
-    private static void handleClient(Socket clientSocket) {
-        try (
-                Scanner scanner = new Scanner(clientSocket.getInputStream());
-                PrintWriter writer = new PrintWriter(new OutputStreamWriter(clientSocket.getOutputStream()), true)
-        ) {
-
-            writer.println("220 Service ready for new user.");
-
-            boolean userAuthenticated = false;
-
-            String clientCommand;
-            while (scanner.hasNextLine()) {
-                clientCommand = scanner.nextLine();
-                System.out.println("Commande reçue: " + clientCommand);
-
-                if (clientCommand.startsWith("USER")) {
-                    String[] parts = clientCommand.split("\\s+");
-                    if (parts.length == 2 && parts[1].equals(USER)) {
-                        userAuthenticated = true;
-                        writer.println("331 User name okay, need password.");
-                    } else {
-                        writer.println("530 Not logged in.");
-                    }
-                } else if (userAuthenticated && clientCommand.startsWith("PASS")) {
-                    String[] parts = clientCommand.split("\\s+");
-                    if (parts.length == 2 && parts[1].equals(PASS)) {
-                        writer.println("230 User logged in.");
-                    } else {
-                        writer.println("530 Login incorrect.");
-                    }
-                } 
-            }
-
-        } catch (IOException e) {
-            e.printStackTrace();
         }
-    }
-}
