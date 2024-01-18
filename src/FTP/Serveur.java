73d06ada112557449193b80bb3f12e40ff93f3bb
lyndabnh <144959102+lyndabnh@users.noreply.github.com>
2024-01-18 20:27:28 +0100

    Add files via upload
    je me suis trompé dans le fichier

--- src/FTP/Serveur.java
+++ src/FTP/Serveur.java
@@ -1,46 +1,75 @@
-package FTP;
-import java.io.*;
-import java.net.*;
+package ftp;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.Scanner;
 
 public class Serveur {
+	private static ServerSocket server;
+	private static Socket client;
 
-    static String USER = "bounehar";
-    static String PASS = "b";
+ public static void main(String[] args) throws IOException{
 
-    public static void main(String[] args) {
-        int port = 2020;
+	 String user = "bounehar";
+	 String pass = "b"; 
 
-        try (ServerSocket serverSocket = new ServerSocket(port)) {
+	 
 
-            System.out.println("Serveur FTP démarré sur le port " + port + "...");
+	 
 
-            
+	 server = new ServerSocket(2121);
+	 System.out.println("Serveur FTP démarré sur le port 2121");
+	 client = server.accept(); // Accepter la connexion
 
-                while (true) { 
-                    Socket clientSocket = serverSocket.accept();
-                    OutputStream out =clientSocket.getOutputStream();
-                    String str="200 Service ready\r\n";
-                    out.write(str.getBytes());
-                  
-                    InputStream in = clientSocket.getInputStream();
-                    
-                    Scanner scanner = new Scanner(in);
+	
+	 InputStream clientSocket = client.getInputStream();
+	 Scanner scan = new Scanner(clientSocket);
+	 OutputStream out = client.getOutputStream();
+	 out.write( "220 Service ready\r\n".getBytes());
 
+	 // user
 
-                    String userName= scanner.nextLine();
-                    System.out.println(userName);
-                    out.write("331 User name ok \r\n".getBytes());
-                 
+	 String login = scan.nextLine();
+	 System.out.println(login);
+	 if(login.equals("USER " +user)){
+	 out.write("331 user name ok \r\n".getBytes());
+	 }else {
+	 String strL = "441 Login invalid\r\n";
+	 out.write( strL.getBytes());
 
-                    String pwd= scanner.nextLine();
-                    out.write("230 User logged in \r\n".getBytes());
-                    System.out.println(pwd);
+	 }
 
-                   scanner.close();
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-    }
-        }
+	// password
+
+	 String pas = scan.nextLine();
+	 System.out.println(pas);
+	 if(pas.equals("PASS " +pass)){
+	 out.write("230 User logged in \r\n".getBytes());
+	 }else {
+     out.write("441 Pass invalid\r\n".getBytes());
+     } 
+	 
+	// quit
+ 
+	 while(true){
+     if (scan.hasNextLine()) {
+     String quit = scan.nextLine();
+     if (quit.equals("QUIT")) {
+     out.write("221 User logged out\r\n".getBytes());
+     } else {
+     out.write("502 Command not implemented\r\n".getBytes());
+     }
+     }
+	 }
+ }
+}
+
+	 
+
+	 
+
+	 
+
+	
